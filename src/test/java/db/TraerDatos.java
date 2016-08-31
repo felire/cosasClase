706784c7614d919200d0723acb5757397f90c8db
Felipe <feliress13@gmail.com>
2016-08-30 21:55:55 -0300

    Anda todo

--- src/test/java/db/TraerDatos.java
+++ src/test/java/db/TraerDatos.java
@@ -8,15 +8,18 @@ import org.junit.Test;
 import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
 
 import jacklow.model.Robo;
+import jacklow.model.Vehiculo;
 
 public class TraerDatos {
 
 	@Test
 	public void test() {
 		EntityManager entityManager = PerThreadEntityManagers.getEntityManager();
+		Vehiculo vehiculo = entityManager.find(Vehiculo.class, 1l);
 		Robo robo = entityManager.find(Robo.class, 1l);
 		
-		//robo.getEventos().stream().forEach(evento->System.out.println(evento.getDescripcion() + "\n"));
+		
+		robo.getEventos().stream().forEach(evento->System.out.println(evento.getDescripcion() + "\n"));
 	}
 
 }
